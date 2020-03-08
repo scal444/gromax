3e9bc3850b470c3acd218470b6d5c72568928941
Kevin Boyd <kevin.boyd@uconn.edu>
2020-03-08 15:48:11 -0700

    Add group serializer and add option to prepend the gmx command

--- gromax/output.py
+++ gromax/output.py
@@ -1,15 +1,18 @@
-from gromax.combination_generator import ParameterSet
-from typing import Any
+from gromax.combination_generator import ParameterSet, ParameterSetGroup
+from typing import Any, List
 
 
-def _serializeParams(params: ParameterSet) -> str:
+def _serializeParams(params: ParameterSet, gmx: str = None) -> str:
     """
-        Turn dictionary of parameters into a string. Orders based on internal python sorting of keys.
+        Turn dictionary of parameters into a string. Orders based on internal python sorting of keys. If the gmx
+        parameter is specified, will prepend the contents.
 
         Note that this is specifically for gromacs parameters, and will append single dashes to keywords, so
         {"ntomp": 80} as input will yield a string '-ntomp 80'
     """
-    kvs = []
+    kvs: List[str] = []
+    if gmx:
+        kvs.append(gmx)
     for key in sorted(params):
         val: Any = params[key]
         if val is not False:
@@ -20,3 +23,16 @@ def _serializeParams(params: ParameterSet) -> str:
             kvs.append(str(val))
 
     return " ".join(kvs)
+
+
+def _serializeConcurrentGroup(param_group: ParameterSetGroup, gmx: str = None) -> str:
+    """
+        Turn a concurrent group of parameters into a line separated bash string, with ampersands to send the initial
+        processes to background for concurrent execution.
+
+        Optionally will prepend contents of gmx to each line.
+    """
+    param_lines: List[str] = []
+    for params in param_group:
+        param_lines.append(_serializeParams(params, gmx))
+    return "&\n".join(param_lines)
--- gromax/tests/output_test.py
+++ gromax/tests/output_test.py
@@ -1,5 +1,5 @@
 import unittest
-from gromax.output import _serializeParams
+from gromax.output import _serializeParams, _serializeConcurrentGroup
 
 
 class SerializeParamsTest(unittest.TestCase):
@@ -29,3 +29,34 @@ class SerializeParamsTest(unittest.TestCase):
     def testKeyValStringFloat(self):
         params = {"pme": "gpu", "maxh": 3.5}
         self.assertEqual(_serializeParams(params), "-maxh 3.5 -pme gpu")
+
+    def testPrependGmx(self):
+        pass
+
+
+class SerializeConcurrentGroupTest(unittest.TestCase):
+    param_group = [
+        {
+            "nt": 4,
+            "pinoffset": 0
+        },
+        {
+            "nt": 4,
+            "pinoffset": 4
+        },
+        {
+            "nt": 4,
+            "pinoffset": 8
+        }
+    ]
+
+    def testEmptyGroup(self):
+        self.assertEqual("", _serializeConcurrentGroup([]))
+
+    def testMultiGroup(self):
+        expected: str = "-nt 4 -pinoffset 0&\n-nt 4 -pinoffset 4&\n-nt 4 -pinoffset 8"
+        self.assertEqual(_serializeConcurrentGroup(self.param_group), expected)
+
+    def testPrependGmx(self):
+        expected: str = "gmx_mpi -nt 4 -pinoffset 0&\ngmx_mpi -nt 4 -pinoffset 4&\ngmx_mpi -nt 4 -pinoffset 8"
+        self.assertEqual(_serializeConcurrentGroup(self.param_group, gmx="gmx_mpi"), expected)
