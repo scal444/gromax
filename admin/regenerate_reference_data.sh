9966b8ca2a1bdb18306322e0ea91c296a19faaa2
Kevin Boyd <kevin.boyd@uconn.edu>
2020-11-09 15:55:55 -0800

    Update admin script
    Added shellcheck to PyCharmm, now need to fix all of it's nits. One
    of them was a real bug, referencing "topdir" instead of $topdir

--- admin/regenerate_reference_data.sh
+++ admin/regenerate_reference_data.sh
@@ -4,7 +4,7 @@
 # tests and overwrites the reference run files the integration tests are run against.
 
 topdir=$(git rev-parse --show-toplevel)
-if [ -z topdir ]; then
+if [ -z "$topdir" ]; then
   echo "Not in a gromax git repo" && exit 1
 fi
 
@@ -14,32 +14,32 @@ RUNBASE="$exe generate --cpu_ids=0-3 --gpu_ids=0,1"
 OUTDIR="$topdir/gromax/tests/integration/testdata"
 
 # Basic 2016 test
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_default_2016.sh --gmx_version=2016
+$PYTHON "$RUNBASE" --run_file="${OUTDIR}"/generate_test_default_2016.sh --gmx_version=2016
 
 # Basic 2018 test
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_default_2018.sh --gmx_version=2018
+$PYTHON "$RUNBASE" --run_file="$OUTDIR"/generate_test_default_2018.sh --gmx_version=2018
 
 # basic 2019 test
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_default_2019.sh --gmx_version=2019
+$PYTHON "$RUNBASE" --run_file="$OUTDIR"/generate_test_default_2019.sh --gmx_version=2019
 
 # custom executable test
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_custom_exe.sh --gmx_version=2016 \
+$PYTHON "$RUNBASE" --run_file="$OUTDIR"/generate_test_custom_exe.sh --gmx_version=2016 \
   --gmx_executable=/path/to/gmx_mpi
 
 # custom trials test
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_custom_ntrials.sh --gmx_version=2016 \
+$PYTHON "$RUNBASE" --run_file="$OUTDIR"/generate_test_custom_ntrials.sh --gmx_version=2016 \
   --trials_per_group=18
 
 # custom directory is ignored for generate
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_default_2016.sh --gmx_version=2016 \
+$PYTHON "$RUNBASE" --run_file="$OUTDIR"/generate_test_default_2016.sh --gmx_version=2016 \
   --directory=/path/to/nowhere
 
 # custom tpr
-$PYTHON $RUNBASE --run_file=$OUTDIR/generate_test_custom_tpr.sh --gmx_version=2016 \
+$PYTHON "$RUNBASE" --run_file="$OUTDIR"/generate_test_custom_tpr.sh --gmx_version=2016 \
   --tpr=custom_tpr.tpr
 
 
 # Test with strange CPU/GPU count that still works
-$PYTHON $exe generate --cpu_ids=0-6 --gpu_ids=0,1 --run_file=$OUTDIR/generate_test_odd_cpu_count.sh --gmx_version=2016 
+$PYTHON "$exe" generate --cpu_ids=0-6 --gpu_ids=0,1 --run_file="$OUTDIR"/generate_test_odd_cpu_count.sh --gmx_version=2016
 
 exit 0
